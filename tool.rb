01c490eead5c7aa249dffdc6abf6f62c4f9e045b
Fletcher Fowler <fletch@fzf.me>
2012-03-15 21:40:38 -0700

    changed wording on exception raise

--- tool.rb
+++ tool.rb
@@ -1,5 +1,5 @@
 class Tool
   def use
-    raise "#{self.class}#use method has not been implemented yet"
+    raise "#{self.class}##{__method__.to_s} has not been implemented, and is required by it's parent"   
   end
 end
